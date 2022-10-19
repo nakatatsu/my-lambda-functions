3493e7597c5f2628fa8439d4cd77e89d29a4da21
nakatatsu <info@tricrow.com>
2022-10-19 21:08:48 +0900

    github actions用ファイル

---
+++ src/send_mail/upload.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# [USAGE]
+# cd /mnt/c/MyFile/GitHub/my-lambda-functions/src/send_mail
+# bash ./upload.sh develop
+
+set -eu
+
+if [ -z "$1" ]; then
+    echo "環境名を引数で指定してください"
+fi
+ 
+ENV=$1
+BUCKET_NAME=$ENV-personal-website-ld4ab847
+FILE_NAME=send_mail-$(date +'%Y%m%d%H%M%S').zip
+FUNCTION_NAME=$ENV-send-mail
+
+# 要件5. アップロードすべきファイルをZipで圧縮し、S3にPUTする。
+zip -j $FILE_NAME ./*.py ./*.txt
+aws s3 cp $FILE_NAME s3://$BUCKET_NAME/lambda/$FILE_NAME
+
+# 要件6. Lambda関数にアップロードする。関数バージョンも同時に作成する。
+aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket $BUCKET_NAME --s3-key lambda/$FILE_NAME --publish
+
+# 要件7. エイリアスをLATESTに更新する。
+aws lambda update-alias --function-name $FUNCTION_NAME --name $FUNCTION_NAME-alias --function-version \$LATEST
+
